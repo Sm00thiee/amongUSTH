9debb28c6f7dd8f28477e9f96d451850b376a9e7
Sm00thiee <nhanlq2013@gmaill.com>
2021-02-22 17:51:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -416,6 +416,10 @@ def read_cert():
     content = open('cert.txt', 'r')
     return Response(content, mimetype='text/plain')
 
+import ssl
+context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+context.load_cert_chain("certificate.crt", "private.key")
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host = '0.0.0.0', port = port)
+    app.run(debug=True, host = '0.0.0.0', port = port, ssl_context=context)
