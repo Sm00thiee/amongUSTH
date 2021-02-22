9b37d1c925043b70ef4a375226cd6fe1892ebe21
Sm00thiee <nhanlq2013@gmaill.com>
2021-02-22 12:27:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -412,4 +412,4 @@ def get_file():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host = '0.0.0.0', ssl_context="adhoc", port = port)
+    app.run(debug=True, host = '0.0.0.0', port = port)
