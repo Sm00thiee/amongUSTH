68c500e54ef686ea1c766ab5ed07e672dca8819f
Sm00thiee <nhanlq2013@gmaill.com>
2021-02-22 17:55:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -422,4 +422,4 @@ context.load_cert_chain("certificate.crt", "private.key")
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host = '0.0.0.0', port = port, ssl_context=context)
+    app.run(host = '0.0.0.0', port = port)
