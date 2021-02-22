6d2da0107d44b169498cbce9970ac20e8796fed3
Sm00thiee <nhanlq2013@gmaill.com>
2021-02-22 18:29:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -422,4 +422,4 @@ def read_cert():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host = '0.0.0.0', port = port, debug='True')
+    app.run(host = '0.0.0.0', port = port)
